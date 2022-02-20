01b33f67dd462bdecec8d0557a7546dd00ada641
Hiroyasu Shimizu <hryssmz@yahoo.com>
2022-02-20 10:23:16 +0900

    chore: Add routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,14 @@
 // routes/index.ts
 import { Router } from "express";
+import { loginApi, homeApi, logoutApi } from "../apis/authApi";
 import { indexApi } from "../apis/indexApi";
 
 const router = Router();
 
 router.get("/", indexApi);
+
+router.post("/login", loginApi);
+router.get("/home", homeApi);
+router.post("/logout", logoutApi);
 
 export default router;
